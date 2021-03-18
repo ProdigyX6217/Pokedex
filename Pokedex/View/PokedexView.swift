2966210b89c8b8822739c3ca6a3ae547edc0a2e7
ProdigyX6217 <dominic.holder.15@cityas.org>
2021-03-18 07:34:54 -0600

    Added PokedexView

---
+++ Pokedex/View/PokedexView.swift
@@ -0,0 +1,33 @@
+//
+//  PokedexView.swift
+//  Pokedex
+//
+//  Created by Student Laptop_7/19_1 on 3/18/21.
+//
+
+import SwiftUI
+
+struct PokedexView: View {
+//  Returns cells per row(2)
+    private let gridItems = [GridItem(.flexible()), GridItem(.flexible())]
+    
+//  Renders Scroll View for Pokemon
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: gridItems, spacing: 16) {
+                    ForEach(0..<151) { _ in
+                        PokemonCell()
+                    }
+                }
+            }
+            .navigationTitle("Pokemon")
+        }
+    }
+}
+
+struct PokedexView_Previews: PreviewProvider {
+    static var previews: some View {
+        PokedexView()
+    }
+}
