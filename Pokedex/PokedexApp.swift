101791e69d0a0c35bd3363aa5eb15097fc587a74
ProdigyX6217 <dominic.holder.15@cityas.org>
2021-03-18 07:26:39 -0600

    Created PokemonCell

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Student Laptop_7/19_1 on 3/18/21.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
